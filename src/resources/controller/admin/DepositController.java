145f13ae04ec1828370fdf523eb041f22792ab21
channoufi-saber <saber.channoufi@esprit.tn>
2024-05-21 22:12:01 +0100

    Client Dashboard

--- src/resources/controller/admin/DepositController.java
+++ src/resources/controller/admin/DepositController.java
@@ -6,7 +6,15 @@ package resources.controller.admin;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import channoufibank.Models.Client;
+import channoufibank.Models.Model;
+import channoufibank.Views.ClientCellFactory;
+import javafx.collections.ObservableList;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 
 /**
  * FXML Controller class
@@ -15,12 +23,39 @@ import javafx.fxml.Initializable;
  */
 public class DepositController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    public TextField pAddress_fld;
+    public Button search_btn;
+    public ListView<Client> result_listview;
+    public TextField amount_fld;
+    public Button deposit_btn;
+
+    private Client client;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+        search_btn.setOnAction(event -> onClientSearch());
+        deposit_btn.setOnAction(event -> onDeposit());
+    }
+
+    private void onClientSearch() {
+        ObservableList<Client> searchResults = Model.getInstance().searchClient(pAddress_fld.getText());
+        result_listview.setItems(searchResults);
+        result_listview.setCellFactory(e -> new ClientCellFactory());
+        client = searchResults.get(0);
+
+    }
+
+    private void onDeposit() {
+        double amount = Double.parseDouble(amount_fld.getText());
+        double newBalance = amount + client.savingsAccountProperty().get().balanceProperty().get();
+        if (amount_fld.getText() != null) {
+            Model.getInstance().getDatabaseDriver().depositSavings(client.pAddressProperty().get(), newBalance);
+        }
+        emptyFields();
+    }
+
+    private void emptyFields() {
+        pAddress_fld.setText("");
+        amount_fld.setText("");
+    }
 }
--- src/resources/controller/client/DashboardController.java
+++ src/resources/controller/client/DashboardController.java
@@ -4,9 +4,19 @@
  */
 package resources.controller.client;
 
+import channoufibank.Models.Model;
+import channoufibank.Models.Transaction;
 import java.net.URL;
+import java.time.LocalDate;
 import java.util.ResourceBundle;
+import javafx.beans.binding.Bindings;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.text.Text;
 
 /**
  * FXML Controller class
@@ -15,12 +25,32 @@ import javafx.fxml.Initializable;
  */
 public class DashboardController implements Initializable {
 
-    /**
-     * Initializes the controller class.
-     */
+    public Text user_name;
+    public Label login_date;
+    public Label checking_bal;
+    public Label checking_acc_num;
+    public Label savings_bal;
+    public Label savings_acc_num;
+    public Label income_lbl;
+    public Label expense_lbl;
+    public ListView<Transaction> transaction_listview;
+    public TextField payee_fld;
+    public TextField amount_fld;
+    public TextArea message_fld;
+    public Button send_money_btn;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
-    }    
-    
+        bindData();
+    }
+
+    private void bindData() {
+        user_name.textProperty().bind(Bindings.concat("Hi, ").concat(Model.getInstance().getClient().firstNameProperty()));
+        login_date.setText("Today, " + LocalDate.now());
+        checking_bal.textProperty().bind(Model.getInstance().getClient().checkingAccountProperty().get().balanceProperty().asString());
+        checking_acc_num.textProperty().bind(Model.getInstance().getClient().checkingAccountProperty().get().accountNumberProperty());
+        savings_bal.textProperty().bind(Model.getInstance().getClient().savingsAccountProperty().get().balanceProperty().asString());
+        savings_acc_num.textProperty().bind(Model.getInstance().getClient().savingsAccountProperty().get().accountNumberProperty());
+    }
+
 }
